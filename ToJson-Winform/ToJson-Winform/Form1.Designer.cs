26a9fb1900ab411c4376cb1842fe991789e09ae7
Steven <dev@donga.ac.kr>
2023-06-10 15:17:44 +0900

    - Project Init

---
+++ ToJson-Winform/ToJson-Winform/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ToJson_Winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_FolderBrowser = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_FolderBrowser
+            // 
+            this.btn_FolderBrowser.Location = new System.Drawing.Point(53, 56);
+            this.btn_FolderBrowser.Name = "btn_FolderBrowser";
+            this.btn_FolderBrowser.Size = new System.Drawing.Size(210, 64);
+            this.btn_FolderBrowser.TabIndex = 0;
+            this.btn_FolderBrowser.Text = "폴더 찾기";
+            this.btn_FolderBrowser.UseVisualStyleBackColor = true;
+            this.btn_FolderBrowser.Click += new System.EventHandler(this.folderAndFileBrowsing);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1521, 1492);
+            this.Controls.Add(this.btn_FolderBrowser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_FolderBrowser;
+    }
+}
+
---
+++ ToJson-Winform/ToJson-Winform/Form1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ToJson_Winform
+{
+    public partial class Form1 : Form
+    {
+        private string loadFolderName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderAndFileBrowsing(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = Application.StartupPath; // 현재 자신의 위치를 디폴트로 설정함.
+            dialog.IsFolderPicker = true; // 폴더 선택도 가능케함.
+            
+            if(dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(dialog.FileName);
+
+                FileAttributes checkAtt = File.GetAttributes(directoryInfo.FullName);
+
+                if(checkAtt == FileAttributes.Directory) // 선택한 경로가 폴더디렉토리 일경우.
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("exception");
+            }
+        }
+    }
+}
